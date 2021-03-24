2e75868dc329a94c7b67dd6b71051a7816e710c7
Schorle1234 <59049115+Schorle1234@users.noreply.github.com>
2021-03-24 12:48:06 +0100

    read first variables

---
+++ Webseite/berechnung.js
@@ -0,0 +1,9 @@
+var roofAngle = document.getElementById("dachwinkel").nodeValue;
+var roofOrientation = document.getElementById("dachausrichtung").nodeValue;
+var roofSurface = document.getElementById("dachflaeche").nodeValue;
+
+var eegCosts = document.getElementById("eeg").nodeValue;
+var electricityCosts = document.getElementById("stromkostem").nodeValue;
+
+var minimumCostsTotal = document.getElementById("minimum").nodeValue;
+var maximumCostsTotal = document.getElementById("maximum").nodeValue;
