b3b07b9f5460914c50ebae622758aa4048b5e0d6
Schorle1234 <59049115+Schorle1234@users.noreply.github.com>
2021-03-24 13:14:17 +0100

    update var ids

--- Webseite/berechnung.js
+++ Webseite/berechnung.js
@@ -1,9 +1,40 @@
-var roofAngle = document.getElementById("dachwinkel").nodeValue;
-var roofOrientation = document.getElementById("dachausrichtung").nodeValue;
-var roofSurface = document.getElementById("dachflaeche").nodeValue;
+var roofAngle = document.getElementById("roofAngle").nodeValue;
+var roofOrientation = document.getElementById("roofOrientation").nodeValue;
+var roofSurface = document.getElementById("roofSurface").nodeValue;
 
-var eegCosts = document.getElementById("eeg").nodeValue;
-var electricityCosts = document.getElementById("stromkostem").nodeValue;
+var dailyElectricityConsumption = document.getElementById("dailyConsumption");
 
-var minimumCostsTotal = document.getElementById("minimum").nodeValue;
-var maximumCostsTotal = document.getElementById("maximum").nodeValue;
+var eegCosts = document.getElementById("eegCostShare").nodeValue;
+var electricityCosts = document.getElementById("electricityCosts").nodeValue;
+
+var minimumCostsTotal = document.getElementById("minimumCostsTotal").nodeValue;
+var maximumCostsTotal = document.getElementById("maximumCostsTotal").nodeValue;
+
+function readElectricityConsumption(n){
+    if(n == "monthly"){
+        var januaryConsumption = document.getElementById("januaryConsumption").nodeValue;
+        var februaryConsumption = document.getElementById("februaryConsumption").nodeValue;
+        var marchConsumption = document.getElementById("marchConsumption").nodeValue;
+        var aprilConsumption = document.getElementById("aprilConsumption").nodeValue;
+        var mayConsumption = document.getElementById("mayConsumption").nodeValue;
+        var juneConsumption = document.getElementById("juneConsumption").nodeValue;
+        var julyConsumption = document.getElementById("julyConsumption").nodeValue;
+        var augustConsumption = document.getElementById("augustConsumption").nodeValue;
+        var septemberConsumption = document.getElementById("septemberConsumption").nodeValue;
+        var octoberConsumption = document.getElementById("octoberConsumption").nodeValue;
+        var novemberConsumption = document.getElementById("novemberConsumption").nodeValue;
+        var decemberConsumption = document.getElementById("decemberConsumption").nodeValue;
+    }
+    else{
+        var yearlyConsumption = document.getElementById("yearlyConsumption").nodeValue;
+        calculateWithYearlyValues(yearlyConsumption);
+    }
+}
+
+function calculateWithMonthlyValues(n){
+
+}
+
+function calculateWithYearlyValues(n){
+
+}
