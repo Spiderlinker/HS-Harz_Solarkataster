fbb30903cee769b23111cbc37cfdb243601f7637
Spiderlinker <support@spiderlinker.de>
2021-04-12 10:20:21 +0200

    php method to get default values

---
+++ Webseite/costs.php
@@ -0,0 +1,8 @@
+<?php
+
+function getValue($key){
+    $defaultValuesRaw = file_get_contents('default_values.json');
+    $defaultValuesJson = json_decode($defaultValuesRaw, true);
+
+    return $defaultValuesJson[$key];
+}
